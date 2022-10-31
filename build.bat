efdb8427e6f98d4cae7fc86f4148e9b0a6770913
Vamsee Achanta <vamsee.achanta@aceengineer.com>
2022-10-31 10:08:37 -0500

    remove redudent file

--- build.bat
+++
@@ -1,7 +0,0 @@
-git commit files and push to origin
-bumpver update --patch
-python -m build
-
-conda activate lmfa
-pip uninstall leveraged-multi-family-analysis
-pip install leveraged-multi-family-analysis
